6508e1b06fda80615fac9c9eaf13daa1868a4f86
Prajwal Pyakurel <ppyakure@tibco.com>
2020-09-10 13:50:12 -0700

    finished second and third qs

---
+++ Question_3/translate/index.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path')
+
+let lang = process.argv[3]; 
+let filename = process.argv[2];
+
+fs.readFile(path.resolve(filename), 'utf-8', (err, data) => {
+    if(err){
+        return console.log(err);
+    }
+
+    let sourceText = JSON.stringify(JSON.parse(data));
+    var sourceLang = 'auto';
+    var targetLang = lang;
+  
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="
+              + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+    fetch(url).then((r) => r.json()).then((data)=> {
+        console.log(data[0][0][0]);
+    })
+    
+})
